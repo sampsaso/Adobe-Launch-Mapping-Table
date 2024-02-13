f41ea53064d51ef7046730ab6d6ae83f8c93beba
SS <39333433+sampsaso@users.noreply.github.com>
2024-02-13 15:34:27 +0200

    Update mappingTable.js
    Remove useless checks and startsWith

--- src/lib/dataElements/mappingTable.js
+++ src/lib/dataElements/mappingTable.js
@@ -12,47 +12,41 @@ module.exports = function (settings) {
 
     function checkValue(dataElemValue) {
         try {
-            for (var i = 0; i < settings.size; i++) {
+            for (var i = 0, l = settings.size; i < l; i++) {
                 var method = settings[i].method;
                 var out = settings[i].output;
                 var inp = settings[i].input;
 
-                if (method === 'exact match') {
+                if (method === 'is true') {
+                    if (dataElemValue === true) {
+                        return out;
+                    }
+                } else if (method === 'is false') {
+                    if (dataElemValue === false) {
+                        return out;
+                    }
+                } else if (method === 'exact match') {
                     if (dataElemValue === inp) {
                         return out;
                     }
                 } else if (method === 'exact match i') {
-                    if (typeof dataElemValue === 'boolean') {
-                        continue;
-                    }
                     if (dataElemValue.toLowerCase() === inp.toLowerCase()) {
                         return out;
                     }
                 } else if (method === 'starts with') {
-                    if (typeof dataElemValue === 'boolean') {
-                        continue;
-                    }
-                    if (dataElemValue.startsWith(inp)) {
+                    // Use regular expression and ignore startsWith
+                    if((new RegExp('^'+inp)).test(inp)) {
                         return out;
                     }
                 } else if (method === 'contains') {
-                    if (typeof dataElemValue === 'boolean') {
-                        continue;
-                    }
-                    if (dataElemValue.indexOf(inp) > -1) {
+                     if (dataElemValue.indexOf(inp) > -1) {
                         return out;
                     }
                 } else if (method === 'regex') {
-                    if (typeof dataElemValue === 'boolean') {
-                        continue;
-                    }
                     if (new RegExp(inp).test(dataElemValue)) {
                         return out;
                     }
                 } else if (method === 'regex matching') {
-                    if (typeof dataElemValue === 'boolean') {
-                        continue;
-                    }
                     var match = dataElemValue.match(new RegExp(inp));
                     if (match !== null) {
                         // We need to go top down to match $12 before matching $1
@@ -61,14 +55,6 @@ module.exports = function (settings) {
                             // Split-join is a way to replace all occurrences
                             out = out.split('$' + j).join(match[j]);
                         }
-                        return out;
-                    }
-                } else if (method === 'is true') {
-                    if (dataElemValue === true) {
-                        return out;
-                    }
-                } else if (method === 'is false') {
-                    if (dataElemValue === false) {
                         return out;
                     }
                 }
@@ -85,7 +71,6 @@ module.exports = function (settings) {
 
     if (typeof settings !== 'undefined' && settings !== null) {
         var dataElemValue = settings.dataElement;
-        addStartsWithToIE();
         if (typeof dataElemValue !== 'undefined' && dataElemValue !== null && Array.isArray(dataElemValue)) {
             return dataElemValue.map(checkValue);
         }
@@ -94,14 +79,5 @@ module.exports = function (settings) {
 };
 
 /**
- * Needed polyfill as IE doesn't support startsWith on strings
+ * IE compatibility is not needed
  */
-var addStartsWithToIE = function () {
-    if (!String.prototype.startsWith) {
-        String.prototype.startsWith = function (searchString, position) {
-            position = position || 0;
-            return this.substr(position, searchString.length) === searchString;
-        };
-    }
-};
-
